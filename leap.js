7c38632579968ff64660ad49ac96b1751dfe5ed1
rakibulVSC <mrakibulhasan755@gmail.com>
2025-10-25 21:14:32 +0600

    add array avearage

--- leap.js
+++ leap.js
@@ -21,4 +21,31 @@ function isLeapYear2(year){
     }
 }
 const leap=isLeapYear2(2400);
-console.log(leap)
+// console.log(leap)
+
+
+
+
+
+function oddAverage(numbers){
+     let odd=[];
+for(const number of numbers){
+
+if(number%2 ===1){
+odd.push(number)
+}
+
+}
+let sum=0;
+for (const number of odd){
+sum= sum + number;
+}
+const count=odd.length;
+console.log(sum);
+const avg=sum/count;
+return avg;
+}
+
+const numbers=[42,46,85,39,26,71,56];
+const average=oddAverage(numbers);
+console.log(average)
