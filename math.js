e6e9627d28d960cc74efddad78be00cd26eef4d6
rakibulVSC <mrakibulhasan755@gmail.com>
2025-10-27 18:03:28 +0600

    add temp number

---
+++ math.js
@@ -0,0 +1,16 @@
+const max=Math.max(1,45,25,98,63,58);
+// console.log(max)
+
+const today=new Date();
+// console.log(today)
+
+let a= 5;
+let b=7;
+
+console.log(a,b)
+// a=b;
+// b=a;
+const temp=a;
+a=b;
+b=temp;
+console.log(a,b)
